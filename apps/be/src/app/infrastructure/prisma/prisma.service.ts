663473d33fee79228837a95608667ef0d5382629
Edward <e.arechwa@gmail.com>
2025-11-16 21:49:04 +0100

    init: add postgres and prisma

---
+++ apps/be/src/app/infrastructure/prisma/prisma.service.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from '@/generated/prisma/client';
+
+import { Injectable, OnModuleInit } from '@nestjs/common';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
